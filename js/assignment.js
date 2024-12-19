dad221ebc7419baf4ba7c1c52a25a12955045090
Atlas-Lofton <gll4392@Hotmail.com>
2024-12-19 16:52:46 -0600

    updated eventlistener. updated string text to look a little better. Created a function for willMeetResolution.

--- js/assignment.js
+++ js/assignment.js
@@ -10,7 +10,7 @@ const submissionBtn = document.getElementById("submission-btn")
 // Declare your variables here.
 const currentYear = "2025";
 
-const resolution = "Read Torah more";
+const resolution = " Read Torah more.";
 const willMeetResolution = "Yes."
 
 function updateYear() {
@@ -22,14 +22,18 @@ function updateResolution() {
     // Update this function 
     resolutionElement.innerText = resolution
 }
+function updatewillMeetResolution () {
+    willMeetResolutionElement.innerText = willMeetResolution
+}
 
 function render() {
     // Finish writing this function
         updateYear();
         updateResolution();
+        updatewillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-    
+    render();
 })
