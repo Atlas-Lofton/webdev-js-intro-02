af2648890becc2a076cf82b0f9d667709c09ad70
Atlas-Lofton <gll4392@Hotmail.com>
2024-12-19 16:49:41 -0600

    updated variable names and render function.

--- js/assignment.js
+++ js/assignment.js
@@ -8,23 +8,28 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const currentYear = "2025";
+
+const resolution = "Read Torah more";
+const willMeetResolution = "Yes."
 
 function updateYear() {
     // write the logic
-
+    yearElement.innerText = currentYear
 }
 
 function updateResolution() {
     // Update this function 
-
+    resolutionElement.innerText = resolution
 }
 
 function render() {
     // Finish writing this function
-
+        updateYear();
+        updateResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    
 })
