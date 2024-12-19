e44a7b7bb2e552862c1862944d7223072ca4c299
Atlas-Lofton <gll4392@Hotmail.com>
2024-12-19 16:56:05 -0600

    commented out javascript.

--- js/assignment.js
+++ js/assignment.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
 const yearElement = document.getElementById("year");
@@ -37,3 +37,4 @@ submissionBtn.addEventListener("click", function () {
     // Finish writing this function
     render();
 })
+*/
